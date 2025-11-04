026fe69b0fe3236050c10ae80fea55269429c284
Тну Тни <nny_noiiiy_octops@mail.ru>
2025-11-05 02:58:09 +0500

    Добавил работу с указателями и структурами. Поправил информацию

---
+++ Misc.zig
@@ -0,0 +1,60 @@
+//Тут будет набор тестов, которые я не смог отнести к определенной тематике
+const std_lib = @import("std");
+const expect  = std_lib.testing.expect;
+
+test "return struct from asm" {
+
+  const structure = asm volatile (
+      \\mov    $20,    %%eax
+      \\shl    $32,    %%rax
+      \\mov    $10,    %%ebx
+      \\or     %%rbx,  %%rax
+      :  [ret] "={rax}" ( -> struct{a:u32, b:u32} ),
+      :
+      : .{.rax = true,
+          .rbx = true } );
+
+  try expect( structure.a == 10 );
+  try expect( structure.b == 20 );
+  
+}
+
+
+test "write struct in memory" {
+
+  var struct2: struct{ a: u64, b: u64 } align(8) = undefined;
+  
+  asm volatile (
+      \\movq   %[field_a],     %%rax
+      \\movq   $10,            (%%rax)
+      \\movq   %[field_b],     %%rax
+      \\movq   $20,            (%%rax)
+      :
+      : [field_a] "m" (&struct2.a),
+        [field_b] "m" (&struct2.b)
+      : .{ .memory = true, .rax = true }
+  );
+
+
+  std_lib.debug.print("{any}", .{struct2} );
+  
+  try expect( struct2.a == 10 );
+  try expect( struct2.b == 20 );
+  
+}
+
+test "write in pointer" {
+
+  var   a:   u64  = 0;
+
+  asm volatile(
+      \\movq    %[ptr],   %%rax
+      \\movq    $42,      (%%rax)
+      : 
+      : [ptr] "r" (&a)
+      : .{ .memory = true }
+  );
+
+  std_lib.debug.print("\n{any}\n", .{a} );
+  
+}
